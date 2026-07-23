2b02dbc13c9dddcbfc3b0c84e87445118ee17e0e
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-07-23 20:16:27 +0200

    test(logic): add unit tests for calculateTotal pure function

---
+++ src/logic/calculate-total.test.ts
@@ -0,0 +1,71 @@
+import type { Service } from "../types/service";
+import { calculateTotal } from "./calculate-total";
+
+describe('calculateTotal', () => {
+
+    it('calculate the total budget with all selected services', () => {
+
+        const services: Service[] = [
+            {
+                "id": "seo",
+                "name": "SEO",
+                "description": "Optimització per a motors de cerca per millorar la visibilitat online",
+                "price": 300
+            },
+            {
+                "id": "ads",
+                "name": "Ads",
+                "description": "Gestió de campanyes de publicitat en plataformes digitals i xarxes socials",
+                "price": 400
+            },
+            {
+                "id": "web",
+                "name": "Web",
+                "description": "Programació d'una web responsive completa",
+                "price": 500
+            }
+        ]
+
+        const result = calculateTotal(services);
+
+        expect(result).toBe(1200);
+    });
+
+    it('returns 0 if no services are selected', () => {
+
+        const services: Service[] = []
+
+        const result = calculateTotal(services);
+
+        expect(result).toBe(0);
+    });
+
+    it('Calculate the total budget with a selected service', () => {
+
+        const services: Service[] = [
+            {
+                "id": "seo",
+                "name": "SEO",
+                "description": "Optimització per a motors de cerca per millorar la visibilitat online",
+                "price": 300
+            }
+        ]
+
+        const result = calculateTotal(services);
+
+        expect(result).toBe(300);
+    });
+});
+
+// describe('Scenario: Calcular el total cuando no hay servicios seleccionados', () => {
+//     // GIVEN
+//     const services: Service[] = [];
+
+//     // WHEN
+//     const result = calculateTotal(services);
+
+//     // THEN
+//     it('Then debería retornar un total de 0', () => {
+//         expect(result).toBe(0);
+//     });
+// });
