273876e7d28293a151b6173eba284221dfaee985
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-07-27 20:32:08 +0200

    feat(ui): conditionally render WebConfigurator when web service is selected

--- src/components/services-list.tsx
+++ src/components/services-list.tsx
@@ -2,30 +2,34 @@ import services from '../data/services.json';
 import { ServiceCard } from './service-card';
 import type { Service } from '../types/service';
 import type { UseWebConfigReturn } from '../hooks/use-web-config';
+import { WebConfigurator } from './web-configurator';
 
 // import { useSelectedServices } from '../hooks/use-selected-services';
 
 interface ServicesListProps {
-    selectedIds: Service["id"][]; 
+    selectedIds: Service["id"][];
     toggleService: (id: Service["id"]) => void;
-    numberPages: UseWebConfigReturn["numberPages"]; 
-    numberLanguages: UseWebConfigReturn["numberLanguages"]; 
+    numberPages: UseWebConfigReturn["numberPages"];
+    numberLanguages: UseWebConfigReturn["numberLanguages"];
     incrementOrDecrement: UseWebConfigReturn["incrementOrDecrement"];
 }
 
-export const ServicesList = ({selectedIds, toggleService, numberPages, numberLanguages, incrementOrDecrement}: ServicesListProps) => {
+export const ServicesList = ({ selectedIds, toggleService, numberPages, numberLanguages, incrementOrDecrement }: ServicesListProps) => {
 
-// const selectedServices = useSelectedServices();    
-// const selectedIds = selectedServices[0];
-// const toggleService = selectedServices[1];
-// const [selectedIds, toggleService] = useSelectedServices();
+    // const selectedServices = useSelectedServices();    
+    // const selectedIds = selectedServices[0];
+    // const toggleService = selectedServices[1];
+    // const [selectedIds, toggleService] = useSelectedServices();
 
     return (
         <fieldset className='flex flex-col gap-5'>
             <legend className='sr-only'>Selecciona els serveis que t'interessin</legend>
             {services.map(service => (
-                <ServiceCard service={service} isSelected={selectedIds.includes(service.id)} onToggle={toggleService} key={service.id} />
-            ) )}
+                <article key={service.id}>
+                    <ServiceCard service={service} isSelected={selectedIds.includes(service.id)} onToggle={toggleService} />
+                    {(service.id === "web" && selectedIds.includes("web")) && <WebConfigurator numberPages={numberPages} numberLanguages={numberLanguages} incrementOrDecrement={incrementOrDecrement} />}
+                </article>
+            ))}
         </fieldset>
     )
 }
